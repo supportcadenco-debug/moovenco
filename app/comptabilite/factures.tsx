381e1ea7fb8449c1c204cdf938d5e4e22b319a24
supportcadenco-debug <support.cadenco@gmail.com>
2026-06-28 17:16:47 +0200

    clean: suppression console.log debug

--- app/comptabilite/factures.tsx
+++ app/comptabilite/factures.tsx
@@ -256,7 +256,6 @@ export default function Factures() {
 
 
   async function envoyerParMail(doc: any) {
-    console.log('envoyerParMail appelé', doc.client_email)
     if (!doc.client_email) {
       alert('Aucun email client renseigné pour ce document.')
       return
